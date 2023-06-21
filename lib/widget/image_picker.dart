dd15dfe48eef8263ff82f42f329718772b902559
muhammadkamrannazir <bsf1802154@ue.edu.pk>
2023-06-21 18:37:42 +0500

    12

--- lib/widget/image_picker.dart
+++ lib/widget/image_picker.dart
@@ -1,20 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UserImagePicker extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
   const UserImagePicker({super.key});
 
   @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _imagesPicker;
+  @override
   Widget build(BuildContext context) {
+    void pickImage() async {
+      final pickedImage = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 50,
+        maxWidth: 150,
+      );
+      setState(() {
+        _imagesPicker = File(pickedImage!.path);
+      });
+    }
+
     return Column(
       children: [
-        const CircleAvatar(),
+         CircleAvatar(
+          radius: 14,
+          backgroundColor: Colors.grey,
+          backgroundImage: FileImage(_imagesPicker!)
+         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: pickImage,
           icon: const Icon(Icons.image),
           label: Text(
             'Add Image',
             style: TextStyle(
-              color: Theme.of(context).primaryColor,
+              color: Theme.of(context).colorScheme.primary,
             ),
           ),
         ),
